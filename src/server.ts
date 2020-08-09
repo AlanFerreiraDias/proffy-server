e469605535a7eaca72502ca7b429a5b52e281f56
AlanFerreiraDias <dias.ferreira.alan@gmail.com>
2020-08-09 07:32:38 -0300

    Change routes implementation to specific file.

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,12 @@
 console.log('Hello World 2');
 
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-
-    console.log(request.query)
-
-    const users = [
-        { 'name': 'Alan' },
-        { 'name': 'Carlos' }
-    ];
-
-    return response.json(users);
-});
+app.use(routes);
 
 app.listen(3333);
