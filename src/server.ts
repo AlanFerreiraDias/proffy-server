6b5d94b1f91bd71cb68a4fcb31e8bf95f96de80d
AlanFerreiraDias <dias.ferreira.alan@gmail.com>
2020-08-09 09:27:02 -0300

    import cors to allow requests from outside addresses

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ console.log('Hello World 2');
 
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 
 app.use(routes);
 
